2e798d169c3b012b2f865baf4177f9dafb2bbee7
fxnaranjo <fxnaranjo@gmail.com>
2024-05-03 10:49:48 -0500

    tracing

--- scripts/01a-tracing-install.sh
+++ scripts/01a-tracing-install.sh
@@ -4,9 +4,12 @@ echo "Installing Tracing dependencies..."
 oc create -f resources/01a-tracing-platform-namespace.yaml
 oc create -f resources/01b-tracing-platform-operatorgroup.yaml
 oc create -f resources/01c-tracing-platform-subscription.yaml
+echo "Waiting 120 seconds..."
 sleep 120
 oc create -f resources/01d-tracing-data-collection-subscription.yaml
+echo "Waiting 60 seconds..."
 sleep 60
 oc create -f resources/01e-tracing-instance.yaml
+echo "Waiting 60 seconds..."
 sleep 60
 echo "Done!"
