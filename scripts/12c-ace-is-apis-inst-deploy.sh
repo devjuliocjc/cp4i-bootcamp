9d1a28f4477052e59982b41c66ada19ccfa3fbb1
fxnaranjo <fxnaranjo@gmail.com>
2024-05-03 13:16:26 -0500

    typo

--- scripts/12c-ace-is-apis-inst-deploy.sh
+++ scripts/12c-ace-is-apis-inst-deploy.sh
@@ -3,7 +3,7 @@
 if [ ! command -v oc &> /dev/null ]; then echo "oc could not be found"; exit 1; fi;
 if [ -z "$CP4I_VER" ]; then echo "CP4I_VER not set, it must be provided on the command line."; exit 1; fi;
 echo "CP4I_VER is set to" $CP4I_VER
-if [ -z "$CP4I_TRACING" ]; then echo "CP4I Tracing is NOT enabled"; else echo "CP4I Tracing is eabled"; fi;
+if [ -z "$CP4I_TRACING" ]; then echo "CP4I Tracing is NOT enabled"; else echo "CP4I Tracing is enabled"; fi;
 read -p "Press <Enter> to execute script..."
 if [ -z "$CP4I_TRACING" ]
 then
